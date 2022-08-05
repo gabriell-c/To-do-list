425a1d750785198ef7a71d12fe6c511462d1964e
Gabriel Cardoso <gabri3lcardoso07@gmail.com>
2022-08-05 00:25:46 -0300

    verificação se o input esta vazio ou não

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,21 +2,28 @@ import * as React from 'react';
 import { Button, Paper, TextField } from '@mui/material';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import { Cont } from './Style'
-import  { useState } from 'react'
+import  { useState, useEffect } from 'react'
 
 export default function BasicTextFields({addTodo}) {
 
   const [text, setText] = useState('')
   const [id, setId] = useState(0)
+  const [disabledCheck, setDisabled] = useState(true)
 
   const createTodo=(texts)=>{
     const todoObj = { texts: texts, id: id };
     setId(id + 1)
     addTodo(todoObj)
     document.getElementById("outlined-basic").value = ''
-
   }
 
+  useEffect(()=>{
+    if(text === ''){
+      setDisabled(true)
+    }else{
+      setDisabled(false)
+    }
+  })
 
   return (
     <Paper>
@@ -25,7 +32,7 @@ export default function BasicTextFields({addTodo}) {
             onChange={(e)=> setText(e.target.value)}
             label="Adicionar tarefa" style={{fontWeight: 'bold', letterSpacing: '1px'}}
             variant="outlined" />
-            <Button id="Btn" onClick={() => createTodo(text)} style={{textTransform: 'none',marginLeft:
+            <Button id="Btn" disabled={disabledCheck} onClick={() => createTodo(text)} style={{textTransform: 'none',marginLeft:
             '1em', height: '100%', fontFamily: 'Poppins', fontWeight: 'bold'}} variant="contained">
             <AddCircleIcon  sx={{ fontSize: 20, marginRight: .5, fontWeight: 'bold'}}/>ADD</Button>
         </Cont>
